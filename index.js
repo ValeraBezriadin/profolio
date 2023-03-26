fff66ba5a14c0e226a9b0c4527815eb665e88e70
ValeraBezriadin <va1era.bezryadin.ua@gmail.com>
2023-03-26 19:37:03 +0300

    add my works

--- index.js
+++ index.js
@@ -1,9 +1,16 @@
 const menuBtn = document.querySelector(".nav__button")
 const menuList = document.querySelector(".nav__list")
-
+const menuItem = document.querySelectorAll(".nav__item")
 
 menuBtn.addEventListener("click", ()=>{
     menuList.classList.toggle("nav__active")
     menuBtn.classList.toggle("nav__button-active");
+})
+
+menuItem.forEach((item )=>{
+    item.addEventListener("click",()=>{
+        menuList.classList.remove("nav__active")
+    menuBtn.classList.remove("nav__button-active");
+    })
 })
 
